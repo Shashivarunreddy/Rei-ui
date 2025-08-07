c2215de26593e570400e5c993b9d52284c6e3c2b
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2025-08-07 21:55:37 +0530

    made few changes

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,13 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Cult-ui",
+  name: "Rei-ui",
   description: "Accessible and customizable components built with Tailwind and Shadcn.",
-  url: "https://eo-n.vercel.app",
+  url: "localhost:3000",
   ogImage:
     "https://avatars.githubusercontent.com/u/124867727?s=400&u=b6b2fe75db74f64ed174f3448cdf7c5c03944db9&v=4",
   links: {
-    x: "https://x.com/ChristianCaneo5",
-    github: "https://github.com/aeonzz/eo-n",
+    x: "",
+    github: "https://github.com/404cult/Rei-ui",
   },
 };
